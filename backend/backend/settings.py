5f92977fb2b04a0886db13368bc6cc89a0f9dca8
kaboussi00 <kautar.abussi@gmail.com>
2024-11-20 16:26:03 +0100

    fix: settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -145,11 +145,6 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-AUTH_USER_MODEL = 'users.User'
-
-# REST_FRAMEWORK = {
-    
-# }
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
