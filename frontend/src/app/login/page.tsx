34c757320af63a7b3e3147f810a7579ea76efaa6
kaboussi00 <kautar.abussi@gmail.com>
2025-01-04 16:00:12 +0100

    feat: create register and login pages

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const LoginIndex:NextPage = () => {
+    return <h1> login kaboussi</h1>
+}
+
+export default LoginIndex;
---
+++ frontend/src/app/register/page.tsx
@@ -0,0 +1,9 @@
+export default function RegisterPage() {
+    return (
+        <div className="h-screen w-screen justify-center flex items-center bg-white">
+            <div className="shadow-xl p-4 bg-white text-black rounded-xl">
+                <h1>Register</h1>
+            </div>
+        </div>
+    )
+}
