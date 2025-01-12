f6f98eec514131c96db6fa7abf66d7388ecb2877
kaboussi00 <kautar.abussi@gmail.com>
2025-01-12 15:41:23 +0100

    feat: POST register data to backend API

--- frontend/src/app/auth/page.tsx
+++ frontend/src/app/auth/page.tsx
@@ -1,14 +1,13 @@
 // import { useState } from "react"
+import Image from "next/image"
 import { RegisterForm } from "../../components/register-form"
 
 export default function RegisterPage() {
     // const [login, setLogin] = useState<boolean>(true)
     return (
-        <div className="h-screen w-screen justify-center flex items-center">
-            <div className="shadow-xl p-4 text-black rounded-xl">
-                <h1>Register</h1>
-                <RegisterForm/>
-            </div>
+        <div className="h-full w-full justify-center flex-col flex items-center space-y-[40px]">
+            <Image src="/logo.svg" alt="logo" width={265} height={170}/>
+            <RegisterForm/>
             {/* {login ? <div></div> : <div></div>} */}
         </div>
     )
--- frontend/src/components/register-form.tsx
+++ frontend/src/components/register-form.tsx
@@ -1,109 +1,148 @@
-"use client"
+"use client";
 
-import { useForm } from "react-hook-form"
-import { Card } from "./ui/card"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "./ui/form"
-import { z } from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Input } from "./ui/input"
-import { Button } from "./ui/button"
+import { useForm } from "react-hook-form";
+import { Card } from "./ui/card";
+import {
+		Form,
+		FormControl,
+		FormField,
+		FormItem,
+		FormLabel,
+		FormMessage,
+} from "./ui/form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-
+export interface FormDataRegister {
+	username: string;
+	password: string;
+	email: string;
+	confirmPassword: string;
+}
 
 export const RegisterForm = () => {
-    const router = useRouter();
-    
-    const formSchema = z.object({
-        username: z.string().min(4).max(10),
-        email: z.string().email("Invalid email address"),
-        password: z.string().regex(
-            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-            "Password must be at least 8 characters long, include at least one uppercase letter, one lowercase letter, one number, and one special character."),
-        confirmPassword: z.string(),
-    }).refine(data => data.password === data.confirmPassword, { 
-        message: "Passwords must match",
-        path: ["confirmPassword"],
-    });
+	const router = useRouter();
+	const [data, setData] = useState('');
 
-    const registerForm = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            username: "",
-            email:"",
-            password: "",
-            confirmPassword: "",
-        },
-    })
+	const formSchema = z
+		.object({
+			username: z.string().min(4).max(10),
+			email: z.string().email("Invalid email address"),
+			password: z
+				.string()
+				.regex(
+					/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+					"Password must be at least 8 characters long, include at least one uppercase letter, one lowercase letter, one number, and one special character."
+				),
+			confirmPassword: z.string(),
+		})
+		.refine((data) => data.password === data.confirmPassword, {
+			message: "Passwords must match",
+			path: ["confirmPassword"],
+		});
 
-    function submitRegister(values: z.infer<typeof formSchema>) {
-    
-        // Handle the form values
-        console.log(values);
+	const registerForm = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			username: "",
+			email: "",
+			password: "",
+			confirmPassword: "",
+		},
+	});
 
-        // Redirect to the dashboard page
-        router.push("/dashboard");
-    }
+	const fetchData = async (formData: FormDataRegister) => {
+		try {
+			const response = await axios.post('http://127.0.0.1:8000/api/auth/sign_up', formData);
+			setData(response.data.message);
+		} catch (error) {
+			console.error('Error fetching data:', error);
+			return;
+		}
+		router.push("/dashboard");
+	};
 
-    return(
-        <Card className="">
-            <Form {...registerForm}>
-                <form onSubmit={registerForm.handleSubmit(submitRegister)}>
-                    <FormField
-                    control={registerForm.control}
-                    name="username"
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>Username</FormLabel>
-                            <FormControl>
-                                <Input placeholder="Enter your username" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                    ></FormField>
-                    <FormField
-                    control={registerForm.control}
-                    name="email"
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>email</FormLabel>
-                            <FormControl>
-                                <Input placeholder="Enter your email" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                    ></FormField>
-                    <FormField
-                    control={registerForm.control}
-                    name="password"
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>password</FormLabel>
-                            <FormControl>
-                                <Input type="password" placeholder="Enter your password" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                    ></FormField>
-                    <FormField
-                    control={registerForm.control}
-                    name="confirmPassword"
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>Confirm password</FormLabel>
-                            <FormControl>
-                                <Input type="password" placeholder="Confirm your Password" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                    ></FormField>
-                    <Button type="submit">sign up</Button>
-                </form>
-            </Form>
-        </Card>
-    )
-}
+
+	function submitRegister(values: z.infer<typeof formSchema>) {
+		// Handle the form values
+		console.log(values);
+
+		// Redirect to the dashboard page
+		fetchData(values);
+	}
+
+	return (
+		<Card className="w-[690px] h-[606px] bg-[#D05F3B] bg-opacity-[18%]">
+			<Form {...registerForm}>
+				<form onSubmit={registerForm.handleSubmit(submitRegister)}>
+					<FormField
+						control={registerForm.control}
+						name="username"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Username</FormLabel>
+								<FormControl>
+									<Input placeholder="Enter your username" {...field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					></FormField>
+					<FormField
+						control={registerForm.control}
+						name="email"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>email</FormLabel>
+								<FormControl>
+									<Input placeholder="Enter your email" {...field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					></FormField>
+					<FormField
+						control={registerForm.control}
+						name="password"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>password</FormLabel>
+								<FormControl>
+									<Input
+										type="password"
+										placeholder="Enter your password"
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					></FormField>
+					<FormField
+						control={registerForm.control}
+						name="confirmPassword"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Confirm password</FormLabel>
+								<FormControl>
+									<Input
+										type="password"
+										placeholder="Confirm your Password"
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					></FormField>
+					<Button type="submit">sign up</Button>
+				</form>
+			</Form>
+		</Card>
+	);
+};
