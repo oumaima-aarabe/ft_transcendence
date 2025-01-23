d071baa21eb527bf7a3534ab0a8594eff45274f3
kaboussi00 <kautar.abussi@gmail.com>
2025-01-23 17:12:38 +0100

    fix: update friends logic (Serialize friends with related user details)

--- backend/friends/views.py
+++ backend/friends/views.py
@@ -8,21 +8,21 @@ from authentication.models import User
 
 
 class Friend_view(APIView):
-    serializer_class = FriendSerializer
     # permission_classes = [permissions.IsAuthenticated]
+    serializer_class = FriendSerializer
 
     def get(self, request):
-        print('request userID: ', self.request.GET.get('userId'))
-        
-        userId = self.request.GET.get('userId')
+
+        userId = request.query_params.get('userId')
         if not userId:
             return Response(
                 {"error": "UserId not found."},
                 status=status.HTTP_400_BAD_REQUEST
             )
-        
-        current_user = User.objects.get(pk=userId)
-        if not current_user:
+
+        try:
+            current_user = User.objects.get(pk=userId)
+        except User.DoesNotExist:
             return Response(
                 {"error": "User not found."},
                 status=status.HTTP_404_NOT_FOUND
@@ -32,13 +32,22 @@ class Friend_view(APIView):
             Q(user=current_user) | Q(friend=current_user),
             state='accepted'
         )
-        if not accepted_friends:
-            return Response(
-                {"data": []},
-                status=status.HTTP_200_OK
-            )
+        
+        # Serialize friends with related user details
+        serialized_friends = []
+        for friend in accepted_friends:
+            if friend.user == current_user:
+                friend_user = friend.friend
+            else:
+                friend.user
+            serialized_friends.append({
+                'id': friend.id,
+                'friend_id': friend_user.id,
+                'username': friend_user.username,
+                'state': friend.state
+            })
+
         return Response(
             {"data": accepted_friends},
             status=status.HTTP_200_OK
         )
-
