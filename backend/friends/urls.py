42b5fdb78198722229d73de5911638e14d716873
kaboussi00 <kautar.abussi@gmail.com>
2025-01-23 17:53:56 +0100

    chore: add urls for friend views

--- backend/friends/urls.py
+++ backend/friends/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import Friend_view
+from .views import (
+    AcceptedFriendView,
+    BlockedFriendView,
+    IncomingFriendRequestView,
+    OutgoingFriendRequestView
+)
 
 
 urlpatterns = [
-    path("/", Friend_view.as_view(), name='friends'),
-    # path("/pending", Friend_view.as_view({'get': 'list'}), name='friends'),
+    path("/", AcceptedFriendView.as_view(), name='accepted_friends'),
+    path("/blocked", BlockedFriendView.as_view(), name='blocked_friends'),
+    path("/invitations", IncomingFriendRequestView.as_view(), name='invitations'),
+    path("/requests", OutgoingFriendRequestView.as_view(), name='requests'),
 ]
