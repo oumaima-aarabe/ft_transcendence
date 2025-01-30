f1f7baffe42712d5432f339f68e359d9b32ce5e0
kaboussi00 <kautar.abussi@gmail.com>
2025-01-30 22:12:42 +0100

    feat: add style to login form component

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -20,11 +20,11 @@ const LoginForm = () => {
 	const formSchema = z.object({
 		email: z.string().email("Invalid email address"),
 		password: z
-        .string()
-        .regex(
+		.string()
+		.regex(
 			/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
 			"Password must be at least 8 characters long, include at least one uppercase letter, one lowercase letter, one number, and one special character."
-        ),
+		),
 	});
 	
 	const loginForm = useForm<z.infer<typeof formSchema>>({
@@ -43,16 +43,23 @@ const LoginForm = () => {
 
 	return (
 	<div>
-		<Card>
+		<Card className="w-[690px] h-[606px] bg-[#751d03] bg-opacity-[18%] p-10 flex justify-center flex-col rounded-3xl border-none backdrop-blur-lg">
+			<div className="flex justify-center">
+				<p className="text-[22px] font-regular text-white leading-relaxed">
+					Join us by entering your information
+				</p>
+			</div>
+			<div className="flex justify-center">
 			<Form {...loginForm}>
-				<form >
+				<form onSubmit={loginForm.handleSubmit(submitLogin)} 
+				className="h-[472px] w-[562px] space-y-[50px]">
 					<FormField
 						control={loginForm.control}
 						name="email"
 						render={({ field }) => (
-							<FormItem>
+							<FormItem className="w-[562px] h-[54px] space-y-2 text-[#EEE5BE]">
 							<FormLabel>email</FormLabel>
-							<FormControl>
+							<FormControl className="text-[#4C4C4C] rounded-3xl bg-[#EEE5BE]">
 								<Input placeholder="Enter your email" {...field} />
 							</FormControl>
 							<FormMessage />
@@ -63,18 +70,28 @@ const LoginForm = () => {
 						control={loginForm.control}
 						name="password"
 						render={({ field }) => (
-							<FormItem>
+							<FormItem className="w-[562px] h-[54px] space-y-2 text-[#EEE5BE]">
 							<FormLabel>email</FormLabel>
-							<FormControl>
+							<FormControl className="text-[#4C4C4C] rounded-3xl bg-[#EEE5BE]">
 								<Input placeholder="Enter your email" {...field} />
 							</FormControl>
 							<FormMessage />
 							</FormItem>
 					)}
 					/>
-					<Button type="submit">Submit</Button>
+					<Button type="submit"
+					className="w-[562px] h-[54px] mt-7 bg-[#40CFB7] hover:bg-[#EEE5BE] rounded-3xl shadow-lg shadow-[#8D361A]">
+						<span className="text-[#c75b37]">sign up</span>
+					</Button>
+						<div className="flex justify-center space-x-1">
+							<p>You already have an account?</p>
+							<button className="text-[#40CFB7] hover:text-[#8D361A] focus:outline-none">
+								Sign in!
+							</button>
+						</div>
 				</form>
 			</Form>
+			</div>
 		</Card>
 	</div>
 	)
