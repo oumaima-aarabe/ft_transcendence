2ecbf1820f491790bfc70b2f52813178d1cc754e
kaboussi00 <kautar.abussi@gmail.com>
2025-02-01 19:39:16 +0100

    feat: add style to login component

--- frontend/src/app/auth/page.tsx
+++ frontend/src/app/auth/page.tsx
@@ -1,15 +1,20 @@
 // import { useState } from "react"
-import Image from "next/image"
-import { RegisterForm } from "../../components/register-form"
-import LoginForm from "@/components/login-form"
+import Image from "next/image";
+import { RegisterForm } from "../../components/register-form";
+import LoginForm from "@/components/login-form";
 
 export default function RegisterPage() {
-    // const [login, setLogin] = useState<boolean>(true)
-    return (
-        <div className="h-full w-full justify-center flex-col flex items-center space-y-[40px]">
-            <Image src="/logo.svg" alt="logo" width={265} height={170}/>
-            {/* <RegisterForm/> */}
-            <LoginForm/> 
-        </div>
-    )
+  // const [login, setLogin] = useState<boolean>(true)
+  return (
+    <div className="h-full w-full justify-center flex-col flex items-center space-y-[40px]">
+      <Image
+        src="/logo.svg"
+        alt="logo"
+        width={265}
+        height={170}
+      />
+      {/* <RegisterForm/> */}
+      <LoginForm />
+    </div>
+  );
 }
--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -13,12 +13,13 @@ import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from './ui/button';
-import Image from 'next/image';
-
+import { Icon } from '@iconify/react';
+import { useState } from 'react';
 
 
 const LoginForm = () => {
 
+	
 	const formSchema = z.object({
 		email: z.string().email("Invalid email address"),
 		password: z
@@ -46,13 +47,13 @@ const LoginForm = () => {
 	return (
 	<div>
 		<Card className="w-[690px] h-[606px] bg-[#751d03] bg-opacity-[18%] p-10 flex justify-center flex-col rounded-3xl border-none backdrop-blur-lg">
-			<div className="flex justify-center space-x-1">
+			<div className="flex justify-center items-center h-[200px] border border-gray-50 space-x-1 text-[#EEE5BE]">
 				<p>You already have an account?</p>
-				<button className="text-[#40CFB7] hover:text-[#8D361A] focus:outline-none">
+				<button className="text-[#40CFB7] hover:text-[#f18662] focus:outline-none">
 					Sign in!
 				</button>
 			</div>
-			<div className="flex justify-center">
+			<div className="flex justify-center h-[300px] border border-red-400">
 			<Form {...loginForm}>
 				<form onSubmit={loginForm.handleSubmit(submitLogin)} 
 				className="h-[472px] w-[562px] space-y-[50px]">
@@ -62,8 +63,15 @@ const LoginForm = () => {
 						render={({ field }) => (
 							<FormItem className="w-[562px] h-[54px] space-y-2 text-[#FFFFFF]">
 							<FormLabel>Email</FormLabel>
-							<FormControl className="text-[#4C4C4C] rounded-3xl bg-[#EEE5BE]">
-								<Input placeholder="Enter your email" {...field} />
+							<FormControl className="text-[#4C4C4C]">
+								<div className="relative">
+									<div className="absolute left-3 top-1/2 transform -translate-y-1/2">
+										<Icon icon="entypo:email" width="20" height="20" />
+									</div>
+									<div>
+										<Input placeholder="Enter your email" className="pl-10 !bg-[#EEE5BE] !text-[#4C4C4C] !rounded-3xl" {...field} />
+									</div>
+								</div>
 							</FormControl>
 							<FormMessage />
 						</FormItem>
@@ -75,8 +83,15 @@ const LoginForm = () => {
 						render={({ field }) => (
 							<FormItem className="w-[562px] h-[54px] space-y-2 text-[#FFFFFF]">
 							<FormLabel>Password</FormLabel>
-							<FormControl className="text-[#4C4C4C] rounded-3xl bg-[#EEE5BE]">
-								<Input placeholder="Enter your email" {...field} />
+							<FormControl className="text-[#4C4C4C]">
+								<div className="relative">
+									<div className="absolute left-3 top-1/2 transform -translate-y-1/2">
+										<Icon icon="icon-park-solid:lock-one" width="24" height="24" />
+									</div>
+									<div>
+										<Input placeholder="Enter your email" className="pl-10 !bg-[#EEE5BE] !text-[#4C4C4C] !rounded-3xl" {...field} />
+									</div>
+								</div>
 							</FormControl>
 							<FormMessage />
 							</FormItem>
@@ -86,18 +101,18 @@ const LoginForm = () => {
 					className="w-[562px] h-[54px] mt-7 bg-[#40CFB7] hover:bg-[#EEE5BE] rounded-3xl shadow-shd">
 						<span className="text-[#c75b37]">sign up</span>
 					</Button>
-					<div className="flex justify-center">
-						<p className="text-[15px] font-regular text-white leading-relaxed">
-							Or sign with
-						</p>
-					</div>
-					<div className="justify-center flex-col flex items-center">
-						<button>
-							<img src="/42.svg" alt="logo" width={69} height={50} className="w-16 h-auto"/>
-						</button>
-					</div>
 				</form>
 			</Form>
+			</div>
+			<div className="flex flex-col justify-center items-center border h-[300px] border-red space-y-4">
+				<div className="flex items-center w-full">
+					<div className="border-t-2 border-[#40CFB7] flex-grow "></div>
+					<p className="text-[15px] font-regular text-white mx-4">Or sign with</p>
+					<div className="border-t-2 border-[#40CFB7] flex-grow "></div>
+				</div>
+				<button>
+					<img src="/42.svg" alt="logo" width={69} height={50} className="w-16 h-auto"/>
+				</button>
 			</div>
 		</Card>
 	</div>
