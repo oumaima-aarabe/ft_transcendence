744ed22e3dec94c6ec41dbc527c391f1b2493406
kaboussi00 <kautar.abussi@gmail.com>
2025-02-14 20:21:33 +0100

    feat: add loading page

---
+++ frontend/src/app/loading/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function loading() {
+    return (
+    <div className="flex items-center justify-center h-screen">
+        <div className="relative h-10 w-1.5 text-[#40CFB7] animate-paddles">
+            <div className="absolute left-0 right-0 top-[15px] mx-auto h-3 w-3 rounded-full bg-[#f18662] animate-ballbounce"></div>
+        </div>
+    </div>
+    )
+}
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,17 +1,38 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	darkMode: ["class"],
+	content: [
+	"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+	"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+	"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
   	extend: {
+		fontFamily: {
+			sans: ['Poppins', 'sans-serif'],
+		  },
+		keyframes: {
+			paddles: {
+			  "0%": { boxShadow: "-25px -10px, 25px 10px" },
+			  "50%": { boxShadow: "-25px 8px, 25px -10px" },
+			  "100%": { boxShadow: "-25px -10px, 25px 10px" },
+			},
+			ballbounce: {
+			  "0%": { transform: "translateX(-20px) scale(1, 1.2)" },
+			  "25%": { transform: "scale(1.2, 1)" },
+			  "50%": { transform: "translateX(15px) scale(1, 1.2)" },
+			  "75%": { transform: "scale(1.2, 1)" },
+			  "100%": { transform: "translateX(-20px)" },
+			},
+		  },
+		  animation: {
+			paddles: "paddles 0.75s ease-out infinite",
+			ballbounce: "ballbounce 0.6s ease-out infinite",
+		},
 		boxShadow: {
 			'shd': '0px 4px 6.2px rgba(208, 95, 59, 1)',
-},
+		},
   		backgroundImage: {
   			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
   			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
